74dfb7fe21992c360ba82bdabc2962c400ac75f8
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2023-12-31 13:36:58 -0500

    add fragments

--- app/src/main/java/com/jdlstudios/programming/MainActivity.kt
+++ app/src/main/java/com/jdlstudios/programming/MainActivity.kt
@@ -2,10 +2,16 @@ package com.jdlstudios.programming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jdlstudios.programming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
