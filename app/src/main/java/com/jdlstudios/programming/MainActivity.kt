69645b502e57ba9383befe001919cf4acc095450
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2023-12-31 15:45:31 -0500

    add navigation button bar

--- app/src/main/java/com/jdlstudios/programming/MainActivity.kt
+++ app/src/main/java/com/jdlstudios/programming/MainActivity.kt
@@ -2,7 +2,10 @@ package com.jdlstudios.programming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.jdlstudios.programming.databinding.ActivityMainBinding
+import com.jdlstudios.programming.ui.views.HomeFragment
+import com.jdlstudios.programming.ui.views.PriorKnowledgeFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.buttonNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId) {
+                R.id.button_home -> makeCurrentFragment(HomeFragment())
+                R.id.button_knowledge -> makeCurrentFragment(PriorKnowledgeFragment())
+            }
+            true
+        }
 
+    }
+
+    private fun makeCurrentFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.nav_host_fragment, fragment)
+            commit()
+        }
     }
 }
