3f014f5945bc931993bd385451dc9dd2a820cd14
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2023-12-31 17:01:00 -0500

    view model to themes

---
+++ app/src/main/java/com/jdlstudios/programming/model/ThemeModel.kt
@@ -0,0 +1,6 @@
+package com.jdlstudios.programming.model
+
+data class ThemeModel(
+    val title: String,
+    val description: String
+)
--- app/src/main/java/com/jdlstudios/programming/ui/views/PriorKnowledgeFragment.kt
+++ app/src/main/java/com/jdlstudios/programming/ui/views/PriorKnowledgeFragment.kt
@@ -1,22 +1,35 @@
 package com.jdlstudios.programming.ui.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.jdlstudios.programming.R
 import com.jdlstudios.programming.databinding.FragmentPriorKnowledgeBinding
+import com.jdlstudios.programming.ui.viewmodels.PriorKnowledgeViewModel
 
 class PriorKnowledgeFragment : Fragment() {
     private lateinit var binding: FragmentPriorKnowledgeBinding
+    // Instancia del ViewModel
+    private val priorKnowledgeViewModel: PriorKnowledgeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPriorKnowledgeBinding.inflate(inflater)
-
-
+        Log.d("asdasd", "lista: as")
+        // Observar los cambios en currentListThemes
+        priorKnowledgeViewModel.currentListThemes.observe(viewLifecycleOwner) {
+            // Actualizar la interfaz de usuario con la nueva lista de temas
+            // Aquí puedes manejar la lógica para mostrar los temas en tu fragmento
+            Log.d("asdasd", "lista: $it")
+            // Por ejemplo, podrías actualizar un RecyclerView o cualquier otro componente de la interfaz de usuario
+        }
 
         return binding.root
     }
