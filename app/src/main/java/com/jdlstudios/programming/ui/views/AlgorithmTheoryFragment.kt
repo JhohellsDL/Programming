5ac25c6d282d9cc82e2981fbed96835db7cbcf62
Jhohells Erick Dianderas Lopez <45276003+JhohellsDL@users.noreply.github.com>
2024-01-07 16:36:27 -0500

    theory to develop algorithm it's ok

--- app/src/main/java/com/jdlstudios/programming/ui/views/AlgorithmTheoryFragment.kt
+++ app/src/main/java/com/jdlstudios/programming/ui/views/AlgorithmTheoryFragment.kt
@@ -1,8 +1,10 @@
 package com.jdlstudios.programming.ui.views
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +17,7 @@ import com.jdlstudios.programming.data.DataSourceTheoryAlgoritm
 import com.jdlstudios.programming.databinding.FragmentAlgorithmTheoryBinding
 import com.jdlstudios.programming.model.TheoryAlgorithmModel
 import com.jdlstudios.programming.ui.adapter.AlgorithmTheoryAdapter
+import com.jdlstudios.programming.util.INTERVAL_IN_MILLIS_TRANSITION
 import com.jdlstudios.programming.util.TIME_IN_MILLIS_TRANSITION
 import kotlin.math.abs
 
@@ -24,12 +27,16 @@ class AlgorithmTheoryFragment : Fragment() {
     private lateinit var handler: Handler
     private lateinit var listt: MutableList<TheoryAlgorithmModel>
     private lateinit var adapter: AlgorithmTheoryAdapter
+
+    private var countDownTimer: CountDownTimer? = null
+    //private val tiempoTotal = TIME_IN_MILLIS_TRANSITION // Tiempo total de la animación en milisegundos
+    //private val intervalo = 70
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentAlgorithmTheoryBinding.inflate(inflater)
-
+        iniciarProgressBar()
         init()
         setUpTransformer()
 
@@ -37,12 +44,35 @@ class AlgorithmTheoryFragment : Fragment() {
             ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
+                reiniciarProgressBar()
                 handler.removeCallbacks(runnable)
                 handler.postDelayed(runnable, TIME_IN_MILLIS_TRANSITION.toLong())
             }
         })
 
         return binding.root
+    }
+
+    fun iniciarProgressBar() {
+        countDownTimer = object : CountDownTimer(TIME_IN_MILLIS_TRANSITION.toLong(), INTERVAL_IN_MILLIS_TRANSITION.toLong()) {
+            override fun onTick(millisUntilFinished: Long) {
+                val progreso = ((TIME_IN_MILLIS_TRANSITION - millisUntilFinished) / INTERVAL_IN_MILLIS_TRANSITION).toInt()
+                binding.progressCarousel.progress = progreso
+            }
+
+            override fun onFinish() {
+                // Puedes agregar lógica adicional al finalizar el tiempo, si es necesario.
+            }
+        }
+
+        countDownTimer?.start()
+    }
+
+    // Reinicia la animación desde cero
+    fun reiniciarProgressBar() {
+        countDownTimer?.cancel()
+        binding.progressCarousel.progress = 0
+        iniciarProgressBar()
     }
 
     override fun onPause() {
--- app/src/main/java/com/jdlstudios/programming/util/Constants.kt
+++ app/src/main/java/com/jdlstudios/programming/util/Constants.kt
@@ -5,3 +5,4 @@ const val ID_EXERCISE = "idExercise"
 
 const val MAX_CHARACTERS = 90
 const val TIME_IN_MILLIS_TRANSITION = 7000
+const val INTERVAL_IN_MILLIS_TRANSITION = 70
